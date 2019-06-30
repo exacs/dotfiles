4c28867248af35e2263c8dd06ab18892c50a482f
Carlos Saito <carlos@saito.style>
2019-06-30 17:26:54 +0200

    Fix

--- .spacemacs-layers/ecmascript/packages.el
+++ .spacemacs-layers/ecmascript/packages.el
@@ -1,10 +1,7 @@
 (defconst ecmascript-packages
   '(lsp-mode))
 
-(defun ecmascript/init-lsp-mode ()
-  (use-package lsp-mode
-    :hook (js-mode . lsp)
-    :commands lsp))
-
 (defun ecmascript/post-init-lsp-mode ()
-  (add-to-list 'lsp-language-id-configuration '(js-mode . "javascript")))
+  (use-package lsp-mode
+    :config
+    (add-to-list 'lsp-language-id-configuration '(js-mode . "javascript"))))
--- .spacemacs-layers/lsp/packages.el
+++ .spacemacs-layers/lsp/packages.el
@@ -1,7 +1,15 @@
 (defconst lsp-packages
-  '(lsp-mode))
+  '(lsp-mode
+    company-lsp))
 
 (defun lsp/init-lsp-mode ()
   (use-package lsp-mode
     :defer t
+    :hook (prog-mode . lsp)
     :commands lsp))
+
+(defun lsp/init-company-lsp ()
+  (use-package company-lsp
+    :defer t
+    :config
+    (spacemacs|defvar-company-backends company-lsp)))
