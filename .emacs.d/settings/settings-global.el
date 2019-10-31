e1e8871035ffed803ff45e102cedfb2cc7d4d2d6
Carlos Saito <carlos@saito.style>
2019-10-31 15:28:54 +0100

    Add counsel M-X and find file

--- .emacs.d/settings/settings-global.el
+++ .emacs.d/settings/settings-global.el
@@ -19,6 +19,8 @@
   (evil-mode 1))
 
 (use-package ivy :ensure t
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file))
   :config
   (ivy-mode 1)
   (setq ivy-use-virtual-buffers t)
