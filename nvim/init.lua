233365d454603138dd758ebdaa67bd6dfdec2cf9
Carlos Saito <carlos@saito.style>
2022-09-18 19:14:12 +0200

    Initial config for nvim

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+vim.o.showmatch = true
+vim.o.number = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+require("plugins")
+
+-- TODO
+-- coq: Autocomplete https://github.com/ms-jpq/coq_nvim
+-- telescope: search with lots of cool stuff https://github.com/nvim-telescope/telescope.nvim#vim-commands
+-- Optional. legendary: search the which-key stuff. https://github.com/mrjones2014/legendary.nvim
+-- Optional. hydra: https://github.com/anuvyklack/hydra.nvim
+-- nvim-dap: Debug Adapter Protocol: https://github.com/mfussenegger/nvim-dap
+-- Project related plugins: https://github.com/rockerBOO/awesome-neovim#project
+-- editorconfig: https://github.com/gpanders/editorconfig.nvim
+-- Formatters: https://github.com/dense-analysis/ale or https://github.com/sbdchd/neoformat
+-- vv: Neovim client for MacOS https://github.com/vv-vim/vv
+--
+-- Treesitter. https://github.com/nvim-treesitter/nvim-treesitter
+--
+-- Example dotfile: https://github.com/wbthomason/dotfiles/blob/linux/neovim/.config/nvim/lua/plugins.lua
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,58 @@
+vim.cmd [[packadd packer.nvim]]
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    -- Theme and lualine
+    use {
+        'Mofiqul/vscode.nvim',
+        config = function ()
+            require("vscode").setup({})
+        end
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {
+             'kyazdani42/nvim-web-devicons', opt = true
+        },
+        config = function ()
+            require("lualine").setup({
+                options = {
+                    theme = 'vscode'
+                }
+            })
+        end
+    }
+
+    -- Which key
+    use {
+        "folke/which-key.nvim",
+        config = function ()
+            local wk = require("which-key")
+            wk.setup {
+                key_labels = {
+                    ["<space>"] = "SPC",
+                    ["<cr>"] = "RET",
+                    ["<esc>"] = "ESC",
+                    ["<bs>"] = "BACK",
+                    ["<tab>"] = "TAB"
+                }
+            }
+            wk.register({
+                ["<space>"] = {
+                    function()
+                        print("hello world")
+                    end,
+                    "Foobar"
+                }
+            }, { prefix = "<leader>" })
+        end
+    }
+end)
