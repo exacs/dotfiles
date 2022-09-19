f14b7ae1789c83fd3439227715cd0babf5ed4a1e
Carlos Saito <carlos@saito.style>
2022-09-19 21:19:41 +0200

    Add new leader commands

--- nvim/lua/user/which-key.lua
+++ nvim/lua/user/which-key.lua
@@ -9,6 +9,32 @@ return function()
       ["<tab>"] = "TAB",
     },
   })
+
+  -- <leader>f = file
+  wk.register({
+    name = "file",
+    f = { "<cmd>Telescope find_files<cr>", "Find files (Telescope)" },
+    F = { "<cmd>Telescope git_files<cr>", "Git files (Telescope)" },
+    b = { "<cmd>Telescope buffers<cr>", "Buffers (Telescope)" },
+  }, { prefix = "<leader>f" })
+
+  -- <leader>c = nvim configuration
+  wk.register({
+    name = "nvim config",
+    i = { "<cmd>:e $MYVIMRC<cr>", "Open 'init.lua'" },
+  }, { prefix = "<leader>c" })
+
+  -- <leader>P = Packer
+  wk.register({
+    name = "Packer",
+    P = { "<cmd>PackerCompile<cr>", "PackerCompile" },
+    C = { "<cmd>PackerClean<cr>", "PackerClean" },
+    S = { "<cmd>PackerSync<cr>", "PackerSync" },
+    I = { "<cmd>PackerInstall<cr>", "PackerInstall" },
+    U = { "<cmd>PackerUpdate<cr>", "PackerUpdate" },
+  }, { prefix = "<leader>P" })
+
+  -- Hello world
   wk.register({
     ["<space>"] = {
       function()
