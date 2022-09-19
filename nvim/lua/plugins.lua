8d3d76d55d3ec12f35e67305fff33171d65b8132
Carlos Saito <carlos@saito.style>
2022-09-19 16:07:59 +0200

    Enforce 2 spaces in lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,78 +7,78 @@ vim.cmd([[
 ]])
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
 
-	-- Format
-	use("gpanders/editorconfig.nvim")
-	use({
-		"mhartington/formatter.nvim",
-		config = function()
-			require("formatter").setup({
-				filetype = {
-					lua = {
-						require("formatter.filetypes.lua").stylua,
-					},
-				},
-			})
-		end,
-	})
+  -- Format
+  use("gpanders/editorconfig.nvim")
+  use({
+    "mhartington/formatter.nvim",
+    config = function()
+      require("formatter").setup({
+        filetype = {
+          lua = {
+            require("formatter.filetypes.lua").stylua,
+          },
+        },
+      })
+    end,
+  })
 
-	-- Theme and lualine
-	use({
-		"Mofiqul/vscode.nvim",
-		config = function()
-			require("vscode").setup({})
-		end,
-	})
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = {
-			"kyazdani42/nvim-web-devicons",
-			opt = true,
-		},
-		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "vscode",
-				},
-			})
-		end,
-	})
+  -- Theme and lualine
+  use({
+    "Mofiqul/vscode.nvim",
+    config = function()
+      require("vscode").setup({})
+    end,
+  })
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = {
+      "kyazdani42/nvim-web-devicons",
+      opt = true,
+    },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "vscode",
+        },
+      })
+    end,
+  })
 
-	-- Which key
-	use({
-		"folke/which-key.nvim",
-		config = function()
-			local wk = require("which-key")
-			wk.setup({
-				key_labels = {
-					["<space>"] = "SPC",
-					["<cr>"] = "RET",
-					["<esc>"] = "ESC",
-					["<bs>"] = "BACK",
-					["<tab>"] = "TAB",
-				},
-			})
-			wk.register({
-				["<space>"] = {
-					function()
-						print("hello world")
-					end,
-					"Foobar",
-				},
-			}, { prefix = "<leader>" })
-		end,
-	})
+  -- Which key
+  use({
+    "folke/which-key.nvim",
+    config = function()
+      local wk = require("which-key")
+      wk.setup({
+        key_labels = {
+          ["<space>"] = "SPC",
+          ["<cr>"] = "RET",
+          ["<esc>"] = "ESC",
+          ["<bs>"] = "BACK",
+          ["<tab>"] = "TAB",
+        },
+      })
+      wk.register({
+        ["<space>"] = {
+          function()
+            print("hello world")
+          end,
+          "Foobar",
+        },
+      }, { prefix = "<leader>" })
+    end,
+  })
 
-	-- Telescope
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		requires = { { "nvim-lua/plenary.nvim" } },
-		config = function()
-			require("telescope").setup({})
-		end,
-	})
+  -- Telescope
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    requires = { { "nvim-lua/plenary.nvim" } },
+    config = function()
+      require("telescope").setup({})
+    end,
+  })
 end)
