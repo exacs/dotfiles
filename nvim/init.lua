69725b153d8c80bc15eb15098680cb8a8d7cd2e7
Carlos Saito <carlos@saito.style>
2022-09-20 10:35:19 +0200

    Delete todos

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,9 +10,6 @@ require("plugins")
 -- Optional. legendary: search the which-key stuff. https://github.com/mrjones2014/legendary.nvim
 -- Optional. hydra: https://github.com/anuvyklack/hydra.nvim
 -- nvim-dap: Debug Adapter Protocol: https://github.com/mfussenegger/nvim-dap
--- Project related plugins: https://github.com/rockerBOO/awesome-neovim#project
 -- vv: Neovim client for MacOS https://github.com/vv-vim/vv
---
--- Treesitter. https://github.com/nvim-treesitter/nvim-treesitter
 --
 -- Example dotfile: https://github.com/wbthomason/dotfiles/blob/linux/neovim/.config/nvim/lua/plugins.lua
