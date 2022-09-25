812964703ad024f37d95379e3268d5be39ce92ba
Carlos Saito <carlos@saito.style>
2022-09-25 21:07:45 +0200

    nvim-cmp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -37,6 +37,28 @@ return require("packer").startup(function(use)
   use({
     "neovim/nvim-lspconfig",
     config = function()
+      require("lspconfig").sumneko_lua.setup({
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
       require("languages.typescript")()
     end,
   })
@@ -108,5 +130,20 @@ return require("packer").startup(function(use)
     config = function()
       require("telescope").setup({})
     end,
+  })
+  use({
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/nvim-cmp",
+      -- VSnip
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+    },
+    config = require("user.cmp"),
   })
 end)
