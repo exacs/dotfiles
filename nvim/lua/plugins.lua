791d461a41cca9fced9f4dd79e35ccacda1b5cad
Carlos Saito <c.saito.style@gmail.com>
2024-05-01 16:34:26 +0200

    Install conform and format plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,16 +7,34 @@
 
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
     dependencies = {
-      'nvim-lua/plenary.nvim'
+      "nvim-lua/plenary.nvim",
     },
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
       { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
       { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" }
-    }
-  }
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+      },
+    },
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+  },
 }
