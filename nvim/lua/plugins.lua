5a3dc8d025b36f5bb2cb4b8171299a0c3e9cdc7f
Carlos Saito <carlos@saito.style>
2024-06-03 15:51:21 +0200

    Update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -26,6 +26,11 @@ return {
     },
   },
   {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
+  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
--- nvim/lua/plugins/keybindings.lua
+++ nvim/lua/plugins/keybindings.lua
@@ -24,6 +24,14 @@ return {
 
         ["<localleader>"] = {
           ["<localleader>"] = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover info" },
+          d = {
+            name = "+diag",
+            d = { "<cmd>lua vim.diagnostic.open_float()<cr>", "Show diagnostic under cursor" },
+            n = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next diag" },
+            p = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev diag" },
+            t = { "<cmd>lua require'telescope.builtin'.diagnostics{}<cr>", "Telescope diagnostics" },
+          },
+          ["."] = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code action" },
           r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
         },
       })
